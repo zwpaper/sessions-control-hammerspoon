df9b714391ef64ad19a1495fa7bba4aad0c59aab
zwPapEr <zw.paper@gmail.com>
2015-07-20 17:45:36 +0800

    update init.lua to an example

---
+++ init_example.lua
@@ -0,0 +1,10 @@
+--Sessions control--
+require("sessions-control-hammerspoon/sessions_control")
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+	sessionsSave(sessions)
+	hs.reload()
+	hs.notify.new({title="Hammerspoon", informativeText='Config loaded'}):send():release()
+end)
+
+
